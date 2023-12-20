e4b44da80188e3717126dc23c357d991dc9ff475
tai03102004 <124711101+tai03102004@users.noreply.github.com>
2023-12-21 00:24:23 +0700

    update controller

--- NewProject/routes/client/tour.route.ts
+++ NewProject/routes/client/tour.route.ts
@@ -1,16 +1,8 @@
-import { Request,Response } from "express";
 import { Router } from "express";
-import Tour from "../../models/tour.model";
+
+import * as controller from "../../controllers/client/tour.controller";
 const router : Router = Router();
 
-router.get('/', async (req: Request , res : Response)  => {
-    const tours = await Tour.findAll({
-        raw : true // Vẽ lại data theo 1 mảng 
-    });
-    console.log(tours);
-    res.render("client/pages/tours/index",{
-        tours: tours,
-    });
-});
+router.get('/',controller.index);
 
 export const tourRoutes: Router = router;
